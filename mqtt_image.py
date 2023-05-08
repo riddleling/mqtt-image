8e0116422422761ba14b2b10bb7b7ea74b8fc630
Riddle Ling <riddle.apple@gmail.com>
2023-05-08 18:09:45 +0800

    update

--- mqtt_image.py
+++ mqtt_image.py
@@ -3,9 +3,10 @@ from paho.mqtt import client as mqtt_client
 import random
 import threading
 import argparse
+import sys
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import GLib, Gtk, GdkPixbuf
+from gi.repository import GLib, Gtk, GdkPixbuf, Gio
 
 
 class MqttImage(Gtk.Window):
@@ -59,14 +60,104 @@ class MqttImage(Gtk.Window):
         loader.close()
 
 
+class AppWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        main_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)     
+        self.add(main_vbox)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-b", "--broker", help="MQTT Broker", default="broker.MQTTGO.io")
-parser.add_argument("-p", "--port", help="MQTT Port", type=int, default=1883)
-parser.add_argument("-t", "--topic", help="MQTT Image topic")
-args = parser.parse_args()
+        main_vbox.set_margin_top(10)
+        main_vbox.set_margin_start(10)
+        main_vbox.set_margin_end(10)
+        main_vbox.set_margin_bottom(10)
 
-window = MqttImage(args.broker, args.port, args.topic)
-window.show_all()
+        hbox1 = Gtk.Box(spacing=10)   
+        broker_label = Gtk.Label()
+        broker_label.set_text("MQTT Broker:")
+        self.broker_entry = Gtk.Entry()
+        self.broker_entry.set_text("broker.MQTTGO.io")
 
-Gtk.main()
+        hbox1.pack_start(broker_label, False, False, 0)
+        hbox1.pack_start(self.broker_entry, True, True, 0)
+        main_vbox.pack_start(hbox1, True, False, 0)
+
+        hbox2 = Gtk.Box(spacing=10)   
+        port_label = Gtk.Label()
+        port_label.set_text("MQTT Port:   ")
+        self.port_entry = Gtk.Entry()
+        self.port_entry.set_text("1883")
+
+        hbox2.pack_start(port_label, False, False, 0)
+        hbox2.pack_start(self.port_entry, True, True, 0)
+        main_vbox.pack_start(hbox2, True, False, 0)
+
+        hbox3 = Gtk.Box(spacing=10)   
+        topic_label = Gtk.Label()
+        topic_label.set_text("Image Topic: ")
+        self.topic_entry = Gtk.Entry()
+        
+        hbox3.pack_start(topic_label, False, False, 0)
+        hbox3.pack_start(self.topic_entry, True, True, 0)
+        main_vbox.pack_start(hbox3, True, False, 0)
+
+        connect_button = Gtk.Button.new_with_label("Connect")
+        connect_button.connect("clicked", self.click_connect_button)
+        main_vbox.pack_start(connect_button, True, False, 0)
+        self.show_all()
+
+    def click_connect_button(self, button):
+        print(self.broker_entry.get_text())
+        print(self.port_entry.get_text())
+        print(self.topic_entry.get_text())
+
+        win = MqttImage(self.broker_entry.get_text(), 
+                        int(self.port_entry.get_text()), 
+                        self.topic_entry.get_text())
+        win.show_all()
+
+
+
+
+class Application(Gtk.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            application_id="site.riddleling.app.mqtt-image",
+            flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
+            **kwargs
+        )
+        self.window = None
+
+    def do_activate(self):
+        if not self.window:
+            self.window = AppWindow(application=self, title="MQTT Image")
+        self.window.present()
+
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        # convert GVariantDict -> GVariant -> dict
+        options = options.end().unpack()
+
+        if "test" in options:
+            # This is printed on the main instance
+            print("Test argument recieved: %s" % options["test"])
+
+        self.activate()
+        return 0
+
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.run(sys.argv)
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-b", "--broker", help="MQTT Broker", default="broker.MQTTGO.io")
+# parser.add_argument("-p", "--port", help="MQTT Port", type=int, default=1883)
+# parser.add_argument("-t", "--topic", help="MQTT Image topic")
+# args = parser.parse_args()
+
+# window = MqttImage(args.broker, args.port, args.topic)
+# window.show_all()
+
+# Gtk.main()
